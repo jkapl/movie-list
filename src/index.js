118351d61484a3e8fde7259aca76f412103703e6
Joel <joelkaplan1@gmail.com>
2019-06-16 09:31:06 -0700

    Include babel webpack and prettier config

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Welcome extends React.component {
-    render() {
-        return <h1>Hello World from React boilerplate</h1>;
-    }
+  render() {
+    return <h1>Hello World from React boilerplate</h1>;
+  }
 }
 
 ReactDOM.render(<Welcome />, document.getElementById('root'));
